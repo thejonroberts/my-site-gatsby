ff45fee328404f3d3a9c21c66f796c089c9f226a
Jon Roberts <jon.roberts.8@gmail.com>
2020-02-02 01:11:33 -0600

    very basic site

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,39 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Layout from '../components/layout'
+import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO title='Jon Roberts: a Software Developer' />
+
+    <p>
+      I am a software developer.
+      <br />I live in Nashville.
+      <br />I trained at
+      {` `}
+      <a href='http://nashvillesoftwareschool.com'>Nashville Software School</a>.
+      <br />I work at <a href='https://www.beachyapp.com'>Beachy</a>.
+      <br />I use Ruby and Javascript.
+    </p>
+
+    <p>
+      In my spare time, I enjoy live music, following politics, biking, camping, motorcycling, and
+      hanging with my pets.
+    </p>
+
+    <p>
+      I hope to have a blog up here before long... In the meantime, you can find me via:
+      <br />
+      <a href='mailto:jon.roberts.8@gmail.com'>Email</a>
+      {` `}
+      <a href='https://github.com/thejonroberts'>GitHub</a>
+      {` `}
+      <a href='https://twitter.com/the_jon_roberts'>Twitter</a>
+      {` `}
+      <a href='https://www.linkedin.com/in/jon-roberts'>LinkedIn</a>
+    </p>
+
   </Layout>
 )
 
