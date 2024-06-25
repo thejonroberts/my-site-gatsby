62de83ff7c42559630c67ee04f4dac8a3971b35f
Jon Roberts <jon.roberts.8@gmail.com>
2024-06-25 17:25:35 -0400

    css organization

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,31 +5,31 @@ import Seo from '../components/seo'
 const IndexPage = () => {
   return (
     <Layout>
-      <p>
+      <p className="py-2">
         I am a software developer.
         <br />I live in Bloomington, IN.
         <br />I trained at
         {` `}
-        <a href='http://nashvillesoftwareschool.com'>Nashville Software School</a>.
-        <br />I worked at <a href='https://www.beachyapp.com'>Beachy</a>.
+        <a className="text-accent" href='http://nashvillesoftwareschool.com'>Nashville Software School</a>.
+        <br />I worked at <a className="text-accent" href='https://www.beachyapp.com'>Beachy</a>.
         <br />I use Ruby and Javascript.
         <br />
         In my spare time, I enjoy live music, following politics, biking, camping, motorcycling, and
         hanging with my pets.
       </p>
 
-      <p>
+      <p className="py-2">
         I hope to have a blog up here before long... In the meantime, you can reach me via:
         <br />
-        <a className='pr-1' href='mailto:jon.roberts.8@gmail.com'>
+        <a className='text-accent pr-1' href='mailto:jon.roberts.8@gmail.com'>
           Email
         </a>
         {` `}
-        <a className='px-1' href='https://github.com/thejonroberts'>
+        <a className='text-accent px-1' href='https://github.com/thejonroberts'>
           GitHub
         </a>
         {` `}
-        <a className='px-1' href='https://www.linkedin.com/in/jon-roberts'>
+        <a className='text-accent px-1' href='https://www.linkedin.com/in/jon-roberts'>
           LinkedIn
         </a>
       </p>
